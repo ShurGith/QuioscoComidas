cd6aa0ebcf0bd78d78c30a8f80227a966f8754fe
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-09-04 18:31:31 +0200

    available

--- components/products/AvailabilityToggle.tsx
+++ components/products/AvailabilityToggle.tsx
@@ -31,7 +31,8 @@ export default function AvailabilityToggle({ product }: AvailabilityToggleProps)
         <input type="checkbox" id={`toggle-${product.id}`} className="sr-only peer"
           defaultChecked={product.available}
           onChange={handleChange}
-          disabled={isPending} />
+          disabled={isPending}
+          name="available" />
         <div className="w-11 h-6 bg-gray-600 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600
          peer-checked:bg-green-600"></div>
         {/* <span className={`ml-3 text-sm font-medium ${product.available ? 'text-green-600' : 'text-red-600'}`}>
